8489a268ed641b04b60169ad11582e20930b51ea
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-03-10 18:33:08 +0000

    Install dependencies and add navbar, footer, shadcn ui components

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { MdNotifications } from 'react-icons/md';
+import { BsSearch } from 'react-icons/bs';
+import { CgMenuLeft, CgMenuRight } from 'react-icons/cg';
+
+import { Discover, HelpCenter, Notification, Profile, Sidebar } from './index';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar;
---
+++ components/Navbar/Profile/Profile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Proilfe = () => {
+  return (
+    <div>Proilfe</div>
+  )
+}
+
+export default Proilfe;
