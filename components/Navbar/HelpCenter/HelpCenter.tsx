37629f6af96b0bfb618fb4d1d60b5f1032980843
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-03-14 08:39:49 +0000

    Update HelpCenter component styling

--- components/Navbar/HelpCenter/HelpCenter.tsx
+++ components/Navbar/HelpCenter/HelpCenter.tsx
@@ -10,7 +10,7 @@ const HelpCenter = () => {
     {name: "Subscription", link: "subscription"},
   ]
   return (
-    <div className="bg-black shadow-md w-48 rounded-lg">
+    <div className="bg-black shadow-md absolute mt-8 w-48 rounded-lg">
       {helpCenter.map((el, i) => (
           <Link key={i + 1} href={{pathname: `${el.link}`}} className="block px-4 py-2 text-white/80 hover:bg-gray-200 hover:text-gray-500">
             {el.name}
