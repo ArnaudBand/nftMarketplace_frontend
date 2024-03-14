5df23854819f127d9483c91e28a9fc2bbe369a02
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-03-14 08:30:24 +0000

    Add links to HelpCenter component

--- components/Navbar/HelpCenter/HelpCenter.tsx
+++ components/Navbar/HelpCenter/HelpCenter.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Link from "next/link";
 
 const HelpCenter = () => {
+  const helpCenter = [
+    {name: "About", link: "about"},
+    {name: "Contact Us", link: "contact-us"},
+    {name: "Sign Up", link: "sign-up"},
+    {name: "Sign In", link: "sign-in"},
+    {name: "Subscription", link: "subscription"},
+  ]
   return (
-    <div>HelpCenter</div>
+    <div className="bg-black shadow-md w-48 rounded-lg">
+      {helpCenter.map((el, i) => (
+          <Link key={i + 1} href={{pathname: `${el.link}`}} className="block px-4 py-2 text-white/80 hover:bg-gray-200 hover:text-gray-500">
+            {el.name}
+          </Link>
+      ))}
+    </div>
   )
 }
 
-export default HelpCenter
+export default HelpCenter;
